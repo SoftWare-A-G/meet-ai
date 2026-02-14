7ba2b8d1d0d6b83a3730b0b5f26b7150a2c78fa8
Anton Kuznetsov <isnifer@gmail.com>
2026-02-14 19:23:31 +0400

    Add missing modules to worker

--- packages/worker/src/app/lib/markdown.ts
+++ packages/worker/src/app/lib/markdown.ts
@@ -1,5 +1,8 @@
+import DOMPurify from 'dompurify'
+import * as marked from 'marked'
+
 export function renderMarkdown(text: string): string {
-  return DOMPurify.sanitize(marked.parse(text, { breaks: true }))
+  return DOMPurify.sanitize(marked.parse(text, { breaks: true }).toString())
 }
 
 export function highlightCode(container: HTMLElement): void {
